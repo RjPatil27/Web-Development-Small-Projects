922a740bc023a6e44d4d302095995e20bd62026f
Rajat Patil <30958676+RjPatil27@users.noreply.github.com>
2020-09-29 00:13:37 +0530

    Update AgeinDays.js

--- Project 1/AgeinDays.js
+++ Project 1/AgeinDays.js
@@ -1,1 +1,18 @@
 //JavaScript file for Project 1
+
+
+//JavaScript Code : Age into Days.
+function AgeinDays(){
+    let birthyear = prompt('What is your birthyear??');
+    var agetodays = (2020 - birthyear) * 365;
+    //console.log(agetodays);
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + agetodays + ' Days old..!!')
+    h1.setAttribute('id','AgeinDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('AgeinDays').remove();
+}
