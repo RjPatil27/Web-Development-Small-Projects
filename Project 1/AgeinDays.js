04f89b2f2adcd25aef3898cafd34923fac64be8d
Rajat Patil <30958676+RjPatil27@users.noreply.github.com>
2020-10-15 21:47:11 +0530

    Update AgeinDays.js

--- Project 1/AgeinDays.js
+++ Project 1/AgeinDays.js
@@ -6,7 +6,7 @@ function AgeinDays(){
     var agetodays = (2020 - birthyear) * 365;
     //console.log(agetodays);
     var h1 = document.createElement('h1');                  //createElement() used to create HTML tags in JS
-    var textAnswer = document.createTextNode('You are ' + agetodays + ' Days old..!!')  //createTextNode() used to create to add Text in HTML tags.
+    var textAnswer = document.createTextNode('You are ' + agetodays + ' Days old..!!')  //createTextNode() used to add Text in HTML tags.
     h1.setAttribute('id','AgeinDays');              //setAttribute() used to set  attributes in HTML tags like ID, Class etc.
     h1.appendChild(textAnswer);                     
     document.getElementById('flex-box-result').appendChild(h1);     //getElementById() used to get HTML ID of specific div or container.
